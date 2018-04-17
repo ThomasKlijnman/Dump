2ba9e1bf062e4b86f54f991b37e81b8e24f6bd3f
Competent Shard <15871270+ThomasKlijnman@users.noreply.github.com>
2018-04-17 21:21:31 +0200

    Rename Powershellscript.ps1 to 76347.ps1

--- Powershellscript.ps1
+++
@@ -1,1 +0,0 @@
-Rename-computer -newname "WINPS01"New-NetIPAddress -InterfaceAlias "Ethernet 0" -IPAddress 192.168.1.11 -AddressFamily IPv4 -PrefixLength 24Set-DnsClientServerAddress -InterfaceAlias "Ethernet 0" -ServerAddresses ("8.8.8.8","192.168.1.1")Set-TimeZone -Id "W. Europe Standard Time"Restart-Computer
